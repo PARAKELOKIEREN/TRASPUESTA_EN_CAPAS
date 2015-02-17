0db2fe44c3e5be8ded5f1af7dfc9fc0ba86f6824
PARAKE <CALVO_831@HOTMAIL.COM>
2015-02-16 20:35:16 -0500

    TRASPUESTA EN CAPAS
    Implementación del ejercicio anterior con un diseño de 4 capas.

---
+++ HOME.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HOME.aspx.cs" Inherits="TRASPUESTA_EN_CAPAS.HOME" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>HOME</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>TRASPUESTA DE UNA MATRIZ</h1>
+    </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="IdRegistro" DataSourceID="ConexionRegistroMatrices">
+            <Columns>
+                <asp:BoundField DataField="IdRegistro" HeaderText="IdRegistro" InsertVisible="False" ReadOnly="True" SortExpression="IdRegistro" />
+                <asp:BoundField DataField="NombreUsuario" HeaderText="NombreUsuario" SortExpression="NombreUsuario" />
+                <asp:BoundField DataField="NombreArchivo" HeaderText="NombreArchivo" SortExpression="NombreArchivo" />
+                <asp:BoundField DataField="FechaSistema" HeaderText="FechaSistema" SortExpression="FechaSistema" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="ConexionRegistroMatrices" runat="server" ConnectionString="<%$ ConnectionStrings:CadenaDeConexion_REGISTRO_MATRICES %>" SelectCommand="SELECT [IdRegistro], [NombreUsuario], [NombreArchivo], [FechaSistema] FROM [REGISTROS]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ HOME.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TRASPUESTA_EN_CAPAS {
+    
+    
+    public partial class HOME {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Control ConexionRegistroMatrices.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource ConexionRegistroMatrices;
+    }
+}
